4945315cec78affe82ab7e1fe4db373f2f0cf039
Juan Contreras <juancontreras@Juans-MacBook-Air-2.local>
2026-03-11 16:32:43 -0500

    created Pitt's randomized  approximation algorithm

---
+++ src/algorithms/approximations/pitt_randomized.py
@@ -0,0 +1,24 @@
+import random
+
+###
+# pitt_randomized_search(graph)
+# Returns a 2-approximation minimum cover
+# Uses maximal matches theorem. Iterates through edges adding non covered ones. 
+# returns a tuple where the first element is the size of the cover
+# and the second element is a list of nodes that cover the graph
+###
+def pitt_randomized_search(graph, seed = 0):
+    random.seed(seed) 
+    cover = set()
+    for edge in graph.edges:
+        node1 = edge[0]
+        node2 = edge[1]
+        if (not (node1 in cover)) and (not (node2 in cover)):
+            # randomly choose between node 1 and node 2
+            choices = [node1, node2]
+            choice = random.choice(choices)
+            cover.add(choice)
+    cover = list(cover)
+    size = len(cover)
+    return (size, cover)
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from graph import Graph, load_graph
 from algorithms.exact_solutions.exhaustive import exhaustive_search
 from algorithms.approximations.maximal_matching import maximal_matching_search
+from algorithms.approximations.pitt_randomized import pitt_randomized_search
 from pathlib import Path
 
 
@@ -20,6 +21,7 @@ print('Result of the Exhaustive Search: ')
 print('Minimum Vertex Cover Size: ' + str(min_vc))
 print('Covers of that size: ')
 print(all_res)
+print()
 
 # Maximal Matching approximation
 res = maximal_matching_search(g)
@@ -29,5 +31,16 @@ print('Result of Maximal Matching: ')
 print('Vertex Cover Size: ' + str(cover_size))
 print('Cover: ')
 print(cover)
+print()
+
+# Pitt's Randomized approximation
+res = pitt_randomized_search(g)
+cover_size = res[0]
+cover = res[1]
+print('Result of Pitt\'s Ranommized Search: ')
+print('Vertex Cover Size: ' + str(cover_size))
+print('Cover: ')
+print(cover)
+print()
 
 
