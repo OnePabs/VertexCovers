f0e39fcfae774916ffa17cea3e637048815daa49
OnePabs <juanpablocontreras1313@gmail.com>
2026-04-18 15:49:22 -0500

    implemented maximal matching using the new approx_graph structure

--- src/algorithms/approximations/maximal_matching.py
+++ src/algorithms/approximations/maximal_matching.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 ###
 # maximal_matching_search(graph)
@@ -7,12 +8,22 @@
 # and the second element is a list of nodes that cover the graph
 ###
 def maximal_matching_search(graph):
-    cover = set()
+    matched = np.array([False]*graph.num_nodes)
+
     # Go through each edge. If it not already covered add both edge nodes to the cover
-    for edge in graph.get_edges():
-        if not any(edge_node_name in cover for edge_node_name in edge.get_node_names()):
-            cover.update(edge.get_node_names())
-    size = len(cover)
+    for edge in graph.get_edges_iterator():
+        node1_id = graph.nodes_ids[edge[0]]
+        node2_id = graph.nodes_ids[edge[1]]
+        if not matched[node1_id] and not matched[node2_id]:
+            matched[node1_id] = True
+            matched[node2_id] = True
+    
+    # Get the indices of the vertices that are included in the matching
+    indices = np.where(matched)[0]
+    #Get the size of the cover
+    size = len(indices)
+    # Get the cover
+    cover = graph.get_nodes_from_indices(indices)
     return (size, cover)
 
 
